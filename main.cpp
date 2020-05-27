5509cb3bb095f24562663d6269c19d4044e0999b
Honghao Li <hh.li99@outlook.com>
2020-05-27 15:32:26 +0800

    [printAST]: using fmt library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include <stdio.h>
 #include <fstream>
+
+#include <fmt/core.h>
+#include <fmt/printf.h>
+
 #include "AST.h"
 #include "codegen.h"
 #include "parser.hpp"
@@ -15,39 +18,46 @@ extern int yyparse();
 std::ofstream astOut;
 int Node::idCount = 0;
 
-//to do: node->info() is not printed
 void printAST(Node *node) {
-    auto children = node->getChildren();
-    std::string info = node->getInfo();
-    for (auto child : children) {
-        if (child)
-	 astOut << "{ key: "<<child->id<<", text: \""<<child->getName()<<"\", fill: \"#f8f8f8\", stroke: \"#000000\","<<"parent:"<<node->id<<" }," << std::endl;
+  auto children = node->getChildren();
+  std::string info = node->getInfo();
+  for (auto child : children) {
+    if (child) {
+      astOut << fmt::sprintf(R"({ key: %d, text: "%s%s%s", fill: "#f8f8f8", stroke: "#000000", parent: %d },)",
+                                child->id,
+                                child->getName(),
+                                info.empty() ? "" : " : ",
+                                info,
+                                node->id)
+             << std::endl;
     }
+  }
 }
 
 void doNothing(Node *node) {}
 
 int main(int argc, char **argv) {
-    std::string sourcePath;
-    auto sourceFile = sourcePath + argv[1];
-    std::cout << "input file: " << sourceFile << std::endl;
-    yyin = fopen(sourceFile.c_str(), "r");
-    yyparse();
+  std::string sourcePath;
+  auto sourceFile = sourcePath + argv[1];
+  std::cout << "input file: " << sourceFile << std::endl;
+  yyin = fopen(sourceFile.c_str(), "r");
+  yyparse();
 
-    auto root = astRoot;
-    //	install system functions
-	installSystemFunctions(root);
-    //visualize
-    astOut.open("ast.json", std::ios::out | std::ios::trunc);
-    astOut << "var nodeDataArray = [ " << std::endl;
-    astOut << "{ key: "<<root->id<<", text: \""<<root->getName()<<"\", fill: \"#f8f8f8\", stroke: \"#000000\" }," << std::endl;
-    std::cout << "begin draw ast" << std::endl;
-    root->traverse(printAST, doNothing);
-    std::cout << "finish draw ast" << std::endl;
-    astOut << "]" << std::endl;
-    astOut.close();
+  auto root = astRoot;
+  //	install system functions
+  installSystemFunctions(root);
+  //visualize
+  astOut.open("ast.json", std::ios::out | std::ios::trunc);
+  astOut << "var nodeDataArray = [ " << std::endl
+         << fmt::sprintf(R"({ key: %d, text: "%s", fill: "#f8f8f8", stroke: "#000000" }, )",
+                         root->id, root->getName()) << std::endl;
+  std::cout << "begin draw ast" << std::endl;
+  root->traverse(printAST, doNothing);
+  std::cout << "finish draw ast" << std::endl;
+  astOut << "]" << std::endl;
+  astOut.close();
 
-    CodeGenContext context;
-    context.generateCode(root, "output.ll");
-    return 0;
+  CodeGenContext context;
+  context.generateCode(root, "output.ll");
+  return 0;
 }
